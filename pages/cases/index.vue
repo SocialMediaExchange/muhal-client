3fc8d374c9d3667b994c8a3acf217bf73e9a96b1
Majd Al-shihabi <majd.al@gmail.com>
2020-05-28 16:03:53 +0300

    get cases from API endpoint

--- pages/cases/index.vue
+++ pages/cases/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <case-card type="case"/>
-    <case-card type="complaint"/>
-    <case-card type="complaint"/>
-    <case-card type="complaint"/>
-    <case-card type="case"/>
+    <case-card type="case" />
+    <case-card type="complaint" />
+    <case-card type="complaint" />
+    <case-card type="complaint" />
+    <case-card type="case" />
+
+    <case-card v-for="_case in cases" v-bind:key="_case.id" :type="_case.type">
+      {{ item.text }}
+    </case-card>
+    
   </div>
 </template>
 
 <script>
-import CaseCard from "~/components/CaseCard.vue";
+import CaseCard from "~/components/CaseCard.vue"
 
 export default {
   head() {
@@ -20,23 +25,26 @@ export default {
   components: {
     CaseCard
   },
-  asyncData(context) {
-    let data = {}
-    return {
-      cases: data
+  async asyncData(context) {
+    try {
+      let cases = await $axios.$get(`/cases/`)
+      return { cases }
+    } catch (e) {
+      return { cases: [] }
     }
+
   },
   data() {
     return {
-      cases: []
+      //cases: []
     }
   },
   methods: {
     shareCase(case_id) {
       console.log(deleted`${case_id.id}`)
-    }
+    },
   }
-};
+}
 </script>
 
 <style>
