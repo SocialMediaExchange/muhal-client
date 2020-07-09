43f3fe6f3790c410d86e415521c2a14646223ab9
Majd Al-shihabi <majd.al@gmail.com>
2020-07-09 12:00:37 +0300

    case card improvements

--- components/CaseCard.vue
+++ components/CaseCard.vue
@@ -1,42 +1,42 @@
 <template>
-  <article class="w-100 w-30-ns ">
-    <div class="grow br3 bg-muhal-grey b ma3">
-    <!-- FIXME need to add date to the card -->
-    <div class="br3 br--top mv0 pv2 ph3" v-bind:style="{ 'background-color': currentStatusColor }"></div>
-    <nuxt-link :to="localePath(`/cases/${case_.id}`)" class="link no-underline hover-sliver muhal-blue">
-      <div class="flex flex-wrap pa3 bt b--black-10 f4 fs-normal">
-        <div
-          class="w-third mb2"
-        >{{ case_.defendants.length > 1 ? $t('defendants') : $t('defendant') }}:</div>
-        <div class="w-two-thirds mb2 ph2-ns">
-          <ul class="list pa0 ma0">
-            <li
-              v-for="(defendant, index) in case_.defendants"
-              v-bind:key="index"
-            >{{ defendant.firstName }} {{ defendant.lastName }}</li>
-          </ul>
-        </div>
-        <div
-          class="w-third mb1"
-        >{{ case_.plaintiffs.length > 1 ? $t('plaintiffs') : $t('plaintiff') }}:</div>
-        <div class="w-two-thirds ph2-ns">
-          <ul class="list pa0 ma0">
-            <li
-              v-for="(plaintiff, index) in case_.plaintiffs"
-              v-bind:key="index"
-            >{{ plaintiff.firstName }} {{ plaintiff.lastName }}</li>
-          </ul>
-        </div>
-        <div class="w-100 f5 measure-narrow normal">
-          <p>{{ case_.summary | truncate(90, "...") }}</p>
-          <!-- <p>{{ case_.currentStatus }}</p>
+  <article class="w-100 w-100-m w-50-ns">
+    <div class="grow br3 bg-white ma3 card fw1">
+      <!-- FIXME need to add date to the card -->
+      <div
+        class="br3 br--top mv0 pv2 ph3"
+        v-bind:style="{ 'background-color': currentStatusColor }"
+      ></div>
+      <nuxt-link
+        :to="localePath(`/cases/${case_.id}`)"
+        class="link no-underline hover-sliver muhal-blue"
+      >
+        <div class="flex flex-wrap pa3 bt b--black-10 f4 fs-normal">
+          <div class="w-100 mb2">
+            <ul class="list pa0 ma0">
+              <li v-for="(defendant, index) in case_.defendants" v-bind:key="index">
+                <span class="mv1 f2 fw6">{{ defendant.firstName }} {{ defendant.lastName }}</span>
+              </li>
+            </ul>
+          </div>
+          <div class="w-100 mb1 bg-muhal-grey-light pa3 f4">
+            <span class="fw7">{{ case_.plaintiffs.length > 1 ? $t('plaintiffs') : $t('plaintiff') }}</span>
+            <ul class="list pa0 ma0">
+              <li
+                v-for="(plaintiff, index) in case_.plaintiffs"
+                v-bind:key="index"
+              >{{ plaintiff.firstName }} {{ plaintiff.lastName }}</li>
+            </ul>
+          </div>
+          <div class="w-100 f5 measure-narrow normal">
+            <p>{{ case_.summary | truncate(90, "...") }}</p>
+            <!-- <p>{{ case_.currentStatus }}</p>
           <p>{{ case_.judge }}</p>
           <p>{{ case_.platformDisplay }}</p>
-          <p>{{ case_.dateOfPublication }}</p> -->
-          <!-- TODO how should the status of the case be rendered? what other fields should be on the card -->
+            <p>{{ case_.dateOfPublication }}</p>-->
+            <!-- TODO how should the status of the case be rendered? what other fields should be on the card -->
+          </div>
         </div>
-      </div>
-    </nuxt-link>
+      </nuxt-link>
     </div>
   </article>
 </template>
@@ -64,7 +64,7 @@ export default {
 
 <style>
 .card {
-  box-shadow: 0 1rem 1.5rem rgba(0, 0, 0, 0.6);
+  box-shadow: 0px 0px 8px 0 rgba(0, 0, 0, 0.2);
   overflow: hidden;
 }
 
@@ -88,8 +88,8 @@ export default {
     ",": "،",
     "defendant": "المدعى عليه/ا",
     "defendants": "المدعى عليهمن",
-    "plaintiff": "المدعي",
-    "plaintiffs": "المدعون",
+    "plaintiff": "المدعيـ/ـة",
+    "plaintiffs": "المدعون/ات",
     "readMore": "... قرائة المزيد >>"
   }
 }
